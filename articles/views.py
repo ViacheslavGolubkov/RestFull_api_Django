bf6b2c6f066dedc40f8520703f2038ace97001cd
Golubkov <golubkov2506@gmail.com>
2022-04-25 10:40:28 +0300

    views

--- articles/views.py
+++ articles/views.py
@@ -1,11 +1,12 @@
 from articles.serializers import ArticleSerializer
 from articles.models import Article
-from articles.permissions import IsAuthorOrReadOnly
+from articles.permissions import IsAuthorOrReadOnly, IsAnon
 from rest_framework import viewsets
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
+
 
     def get_queryset(self):
         username = self.request.user
@@ -14,5 +15,6 @@ class ArticleViewSet(viewsets.ModelViewSet):
         return Article.objects.filter(is_public=True)
 
     def get_permissions(self):
-        return IsAuthorOrReadOnly(),
-
+        if not self.request.user.is_active:
+            return (IsAnon(),)
+        return (IsAuthorOrReadOnly(),)
